745e9cbbde5ba1dd41d349aebad0bf76d5e2ea55
JeongSeonggil <dataofsg02@gmail.com>
2022-05-22 14:22:24 +0900

    [#9] todo : IP 수정

--- UserService/src/main/java/com/submarket/userservice/security/WebSecurity.java
+++ UserService/src/main/java/com/submarket/userservice/security/WebSecurity.java
@@ -22,7 +22,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-
+        // TODO: 2022/05/22 Gateway IP 로 수정
         http.authorizeRequests().antMatchers("/**")
                 .hasIpAddress("127.0.0.1") // IP
                 .and()
