8a87eac725d260e93a520dd6a5471c704478c29b
JeongSeonggil <dataofsg02@gmail.com>
2022-05-22 14:32:35 +0900

    [#11] feature : Mail 전송

---
+++ UserService/src/main/java/com/submarket/userservice/service/impl/MailService.java
@@ -0,0 +1,34 @@
+package com.submarket.userservice.service.impl;
+
+import com.submarket.userservice.jpa.SubRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service(value = "MailService")
+@RequiredArgsConstructor
+public class MailService {
+    private final Environment env;
+    private final JavaMailSender javaMailSender;
+
+    public void sendMail(String mailAddress, String title, String mailMessage) {
+        log.info(this.getClass().getName() + ".SendMail Start!");
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(env.getProperty("spring.mail.username"));
+        message.setTo(mailAddress);
+        message.setSubject(title);
+        message.setText(mailMessage);
+
+        javaMailSender.send(message);
+
+        log.info(this.getClass().getName() + ".SendMail End!");
+    }
+
+}
