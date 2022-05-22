125454bb4ff91830769dfef7bcea93f28a024294
JeongSeonggil <dataofsg02@gmail.com>
2022-05-22 16:25:03 +0900

    [#18] feature : 상품 등록

---
+++ ItemService/src/main/java/com/submarket/itemservice/service/IItemService.java
@@ -0,0 +1,13 @@
+package com.submarket.itemservice.service;
+
+import com.submarket.itemservice.dto.GroupDto;
+import com.submarket.itemservice.dto.ItemDto;
+
+import java.util.List;
+
+public interface IItemService {
+    int saveItem(ItemDto itemDto) throws Exception;
+
+
+
+}
