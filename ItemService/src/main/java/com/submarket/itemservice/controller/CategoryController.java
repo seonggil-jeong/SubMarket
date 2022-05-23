5c1f3ace2aecba8e9f8167132d43650bff499a60
JeongSeonggil <dataofsg02@gmail.com>
2022-05-23 09:36:56 +0900

    [#31] feature : findItemInfoByCategorySeq

--- ItemService/src/main/java/com/submarket/itemservice/controller/CategoryController.java
+++ ItemService/src/main/java/com/submarket/itemservice/controller/CategoryController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -28,5 +29,21 @@ public class CategoryController {
         log.info(this.getClass().getName() + ">findAllCategory End!");
 
         return ResponseEntity.ok().body(categoryDtoList);
+    }
+
+    @GetMapping("/category/{categorySeq}")
+    public ResponseEntity<CategoryDto> findItemInfoByCategorySeq(@PathVariable int categorySeq) throws Exception {
+        log.info(this.getClass().getName() + ".findItemInfoByCategorySeq Start!");
+
+        log.info("categorySeq : " + categorySeq);
+        CategoryDto pDto = new CategoryDto();
+        pDto.setCategorySeq(categorySeq);
+
+        CategoryDto categoryDto = categoryService.findCategory(pDto);
+
+        log.info("categoryName : " + categoryDto.getCategoryName());
+
+        log.info(this.getClass().getName() + ".findItemInfoByCategorySeq End!");
+        return ResponseEntity.ok().body(categoryDto);
     }
 }
