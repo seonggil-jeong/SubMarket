ac6052b5df35adc70591e8f99b1ad08e197b55a8
JeongSeonggil <dataofsg02@gmail.com>
2022-05-23 10:29:42 +0900

    [#40] fix : TokenUtil

--- UserService/src/main/java/com/submarket/userservice/util/TokenUtil.java
+++ UserService/src/main/java/com/submarket/userservice/util/TokenUtil.java
@@ -1,19 +1,23 @@
 package com.submarket.userservice.util;
 
 import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 
 @Slf4j
+@RequiredArgsConstructor
 public class TokenUtil {
+    private static Environment env;
 
-    public static String getUserIdByToken(HttpHeaders headers, String secret) {
+    public static String getUserIdByToken(HttpHeaders headers) {
         log.debug("Token Util Start!");
         String token = headers.get("Authorization").get(0); // Get Token in headers
         String jwt = token.replace("Bearer", ""); // delete Bearer
 
         log.info("JWT : " + jwt); // TokenValue
-        String userId = Jwts.parser().setSigningKey(secret).parseClaimsJws(jwt).getBody().getSubject();
+        String userId = Jwts.parser().setSigningKey(env.getProperty("token.secret")).parseClaimsJws(jwt).getBody().getSubject();
 
         log.info("getUserId End");
 
