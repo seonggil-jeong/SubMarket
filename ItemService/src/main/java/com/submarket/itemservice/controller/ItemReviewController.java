ee333e74d64da20e0376f6fbfd0e1630411ccf64
JeongSeonggil <dataofsg02@gmail.com>
2022-06-01 23:49:05 +0900

    [#106] refactor : itemService/review

--- ItemService/src/main/java/com/submarket/itemservice/controller/ItemReviewController.java
+++ ItemService/src/main/java/com/submarket/itemservice/controller/ItemReviewController.java
@@ -3,6 +3,7 @@ package com.submarket.itemservice.controller;
 import com.submarket.itemservice.dto.ItemReviewDto;
 import com.submarket.itemservice.service.impl.ItemReviewService;
 import com.submarket.itemservice.util.CmmUtil;
+import com.submarket.itemservice.util.DateUtil;
 import com.submarket.itemservice.util.TokenUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,9 +25,14 @@ public class ItemReviewController {
     private final TokenUtil tokenUtil;
 
     @PostMapping("/item/{itemSeq}/review")
-    public ResponseEntity<String> saveReview(@RequestBody ItemReviewDto itemReviewDto, @PathVariable int itemSeq) throws Exception {
+    public ResponseEntity<String> saveReview(@RequestHeader HttpHeaders headers,
+                                             @RequestBody ItemReviewDto itemReviewDto, @PathVariable int itemSeq) throws Exception {
         log.info(this.getClass().getName() + ".saveReview Start!");
         // TODO: 2022-05-16 사용자가 이미 작성한 리뷰가 있는지 확인 with UserSeq
+
+        String userId = CmmUtil.nvl(tokenUtil.getUserIdByToken(headers));
+        itemReviewDto.setUserId(userId);
+        itemReviewDto.setReviewDate(DateUtil.getDateTime("yyyyMMdd"));
 
         if (itemReviewDto.equals(null)) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("리뷰 정보를 입력해주세요");
@@ -40,7 +46,7 @@ public class ItemReviewController {
         return ResponseEntity.status(HttpStatus.CREATED).body("리뷰 작성 완료");
     }
 
-    @PatchMapping("/item/review/{reviewSeq}")
+    @PostMapping("/item/review/{reviewSeq}/modify")
     public ResponseEntity<String> modifyItemReview(@RequestBody ItemReviewDto itemReviewDto, @PathVariable int reviewSeq)
         throws Exception {
         log.info(this.getClass().getName() + ".modifyItemReview Start!");
@@ -54,7 +60,7 @@ public class ItemReviewController {
         return ResponseEntity.status(HttpStatus.OK).body("리뷰 변경 완료");
     }
 
-    @DeleteMapping("/item/review/{reviewSeq}")
+    @PostMapping("/item/review/{reviewSeq}/delete")
     public ResponseEntity<String> deleteItemReview(@PathVariable int reviewSeq) throws Exception {
         log.info(this.getClass().getName() + ".deleteReview Start!");
 
