ee9fac2a077903249da43d091b86eec6a332e266
JeongSeonggil <dataofsg02@gmail.com>
2022-06-03 13:24:53 +0900

    [#91] feature : S3 Test ( Image Upload )

--- ItemService/src/main/java/com/submarket/itemservice/controller/MainController.java
+++ ItemService/src/main/java/com/submarket/itemservice/controller/MainController.java
@@ -4,11 +4,17 @@ import com.submarket.itemservice.jpa.CategoryRepository;
 import com.submarket.itemservice.jpa.ItemRepository;
 import com.submarket.itemservice.jpa.ItemReviewRepository;
 import com.submarket.itemservice.service.impl.ItemService;
+import com.submarket.itemservice.service.impl.S3Service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @Slf4j
@@ -20,6 +26,7 @@ public class MainController {
     private final ItemService itemService;
     private final ItemRepository itemRepository;
     private final ItemReviewRepository itemReviewRepository;
+    private final S3Service s3Service;
 
     @GetMapping("/health")
     public String health() {
@@ -29,5 +36,13 @@ public class MainController {
                 + ", port(server.port) : " + env.getProperty("server.port")
                 + ", token secret : " + env.getProperty("token.secret")
                 + ", token expiration time : " + env.getProperty("token.expiration_time");
+    }
+
+    @PostMapping("/images")
+    public String upload(@RequestParam("images") MultipartFile multipartFile) throws IOException {
+
+        String rStr = s3Service.uploadImageInS3(multipartFile, "static");
+
+        return rStr;
     }
 }
