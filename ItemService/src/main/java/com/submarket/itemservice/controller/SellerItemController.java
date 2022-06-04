25dd889a59d63159c4928a902cae6ace49bd882c
JeongSeonggil <dataofsg02@gmail.com>
2022-06-04 16:27:41 +0900

    [#105] feature : Seller가 등록한 모든 상품 조회

---
+++ ItemService/src/main/java/com/submarket/itemservice/controller/SellerItemController.java
@@ -0,0 +1,40 @@
+package com.submarket.itemservice.controller;
+
+import com.submarket.itemservice.dto.ItemDto;
+import com.submarket.itemservice.service.impl.ItemService;
+import com.submarket.itemservice.util.TokenUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class SellerItemController {
+    private final ItemService itemService;
+    private final TokenUtil tokenUtil;
+
+
+    @GetMapping("/item/seller")
+    public ResponseEntity<Map<String, Object>> findItemInfoBySellerId(@RequestHeader HttpHeaders headers) throws Exception {
+        log.info(this.getClass().getName() + "findItemInfoBySellerId Start!");
+        String sellerId = tokenUtil.getUserIdByToken(headers);
+        Map<String, Object> rMap = new HashMap<>();
+
+        List<ItemDto> itemDtoList = itemService.findItemBySellerId(sellerId);
+
+        rMap.put("response", itemDtoList);
+
+        log.info(this.getClass().getName() + "findItemInfoBySellerId End!");
+        return ResponseEntity.status(HttpStatus.OK).body(rMap);
+    }
+}
