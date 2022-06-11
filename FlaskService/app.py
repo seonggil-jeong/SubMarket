38067cd478a710cde8bf8b67934f64cb911e925a
JeongSeonggil <dataofsg02@gmail.com>
2022-06-11 16:00:05 +0900

    [#126] setting : Prophet

--- FlaskService/app.py
+++ FlaskService/app.py
@@ -1,13 +1,19 @@
 from flask import Flask
 import pandas as pd
-import py_eureka_client.eureka_client as eureka_client
-# pip install py-eureka-client
+from prophet import Prophet
 
 rest_port = 5000
-eureka_client.init(eureka_server="http://localhost:8761/eureka",
-                   app_name="flask-service",
-                   instance_ip="192.168.170.169",
-                   instance_port=rest_port)
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
+
 app = Flask(__name__)
 
 
