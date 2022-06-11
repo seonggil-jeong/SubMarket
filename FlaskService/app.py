1e0f315a2fe965cc2a8cd198c7951522ad694b69
JeongSeonggil <dataofsg02@gmail.com>
2022-06-11 16:26:29 +0900

    [#126] setting : Eureka

--- FlaskService/app.py
+++ FlaskService/app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 import pandas as pd
 from prophet import Prophet
+import py_eureka_client.eureka_client as eureka_client
+
+rest_port = 5000
+eureka_client.init(eureka_server="http://localhost:8761/eureka",        # Eureka Server Address
+                   app_name="flask-service",        # Service Name
+                   instance_ip="127.0.0.1",     # 등록될 Ip
+                   instance_port=rest_port)     # prot
 
 rest_port = 5000
 app = Flask(__name__)
