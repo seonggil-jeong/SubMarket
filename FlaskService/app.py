9ccbdb88896218d9ecea3c93bb98405eca1ac9c8
JeongSeonggil <dataofsg02@gmail.com>
2022-06-12 12:25:06 +0900

    [#118] feature : EndPoint : /flask-service/seller/analysis

--- FlaskService/app.py
+++ FlaskService/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import pandas as pd
 from prophet import Prophet
 import py_eureka_client.eureka_client as eureka_client
+import numpy as np
 
 rest_port = 5000
 eureka_client.init(eureka_server="http://localhost:8761/eureka",        # Eureka Server Address
@@ -17,17 +18,33 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route('/seller/analysis')
+def seller_value():
+    parameter = request.get_json()
+    value_list = parameter["response"]
+
+    ds = []
+    y = []
+    for value in value_list:
+        print("date : ", value.get("date"))
+        print("value : ", value.get("value"))
+        ds.append(pd.to_datetime(value.get("date")))
+        y.append(value.get("value"))
+
+    s_ds = pd.Series(ds, name="ds")
+    s_y = pd.Series(y, name="y")
+    df = pd.DataFrame(s_ds).join(s_y)
+
+    m = Prophet()
+    m.fit(df)
+
+    future = m.make_future_dataframe(periods=31)
+    forecast = m.predict(future)
+
+    result = int(np.mean(forecast.tail(31)["yhat"].values))
+    print("result : ", result)
+    return str(result)
+
 
 if __name__ == '__main__':
-    app.run()
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=rest_port)
